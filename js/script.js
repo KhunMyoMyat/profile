f62fbd32308228d9c838f401d5dbf4f245162007
Khun Myo Myat <hanmyo551@gmail.com>
2025-03-28 10:08:05 +0700

    menu bar toggle, nav  position fixed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 130) {
+    nav.classList.add("bg-white");
+  } else {
+    nav.classList.remove("bg-white");
+  }
+});
+
+const toggle = document.querySelector("#toggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", function () {
+  menu.classList.toggle("active");
+});
